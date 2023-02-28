34228515c98908d6e53a8c31651e8984e453d4e4
julie <julie@DESKTOP-QBSFADO>
2023-02-28 21:55:44 +0100

    Création du dépot

--- src/projet/cinema/Etablissement.java
+++ src/projet/cinema/Etablissement.java
@@ -25,4 +25,3 @@ public class Etablissement {
     
 }
  
-pipi caca
--- src/projet/cinema/PortailDeCinema.java
+++ src/projet/cinema/PortailDeCinema.java
@@ -1,23 +1,35 @@
 package projet.cinema;
 
+/*
+Projet Portail de Cinema
+BORDIER Julien et ANSALDI Enzo
+*/
+
 import java.util.Scanner;
 
-/**
- *
- * @author julien
- */
+
 public class PortailDeCinema {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Tapez ici votre rechecrhe : ");
-        String Recherche = sc.nextLine();
-        System.out.println("Votre recherche : " + Recherche);
+        System.out.println("Film (1) ou Cinema (2)?");
+        int Recherche = sc.nextInt();
+        
+        if (Recherche == 1){
+            System.out.println("Quel film recherchez-vous?");
+            sc = new Scanner(System.in);
+            String RechercheFlm = sc.nextLine();
+            System.out.println("Votre recherche : " + RechercheFlm);
+        }
+        
+        else {
+            System.out.println("Quelle salle de cinéma cherchez-vous?");
+            sc = new Scanner(System.in);
+            String RechercheSalle = sc.nextLine();
+            System.out.println("Votre recherche : " + RechercheSalle);
+        }
         
         System.out.println("Résultats de la recherche : ");
-
-    
-        
     }
     
 }
